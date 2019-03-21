58210b54d1b6c74441db83661c9f71b2f7da5821
Fabian Wssing <fw.wissing645@gmail.com>
2019-03-21 17:54:39 +0100

    Better Form

--- SQLisland/Form1.Designer.cs
+++ SQLisland/Form1.Designer.cs
@@ -75,7 +75,7 @@
             // 
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Controls.Add(this.panel2);
-            this.panel1.Location = new System.Drawing.Point(26, 84);
+            this.panel1.Location = new System.Drawing.Point(26, 67);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(310, 456);
             this.panel1.TabIndex = 5;
@@ -132,7 +132,7 @@
             this.lbl_mIndex.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(50)))));
             this.lbl_mIndex.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_mIndex.ForeColor = System.Drawing.Color.Red;
-            this.lbl_mIndex.Location = new System.Drawing.Point(247, 46);
+            this.lbl_mIndex.Location = new System.Drawing.Point(368, 24);
             this.lbl_mIndex.Name = "lbl_mIndex";
             this.lbl_mIndex.Size = new System.Drawing.Size(85, 20);
             this.lbl_mIndex.TabIndex = 9;
@@ -144,7 +144,7 @@
             this.lbl_mission.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(50)))));
             this.lbl_mission.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_mission.ForeColor = System.Drawing.Color.Red;
-            this.lbl_mission.Location = new System.Drawing.Point(175, 46);
+            this.lbl_mission.Location = new System.Drawing.Point(296, 24);
             this.lbl_mission.Name = "lbl_mission";
             this.lbl_mission.Size = new System.Drawing.Size(66, 20);
             this.lbl_mission.TabIndex = 10;
@@ -164,7 +164,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.Control;
-            this.label1.Location = new System.Drawing.Point(158, 16);
+            this.label1.Location = new System.Drawing.Point(158, 13);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(599, 20);
             this.label1.TabIndex = 0;
